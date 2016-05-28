dd91b6b619aac3795a3ef919c9278f239898e578
norbyte <infernorb@gmail.com>
2016-05-28 20:33:17 +0200

    Add column type test

---
+++ test/enigma/types.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'connect.inc';
+
+$rows = querya("select 1 as a, 9223372036854775807 as aa, 2.5::float as b, 2.5 as bb, false as c, true as d, 'aaaa' as e, null as f");
+var_dump($rows);
